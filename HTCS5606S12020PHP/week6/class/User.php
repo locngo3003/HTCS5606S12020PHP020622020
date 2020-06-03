2c9a470b23b077e3ff881b2d54a949cd3e327f90
locngo3003 <locnxse04549@gmail.com>
2020-06-04 09:52:35 +1200

    dbconn

--- HTCS5606S12020PHP/week6/class/User.php
+++ HTCS5606S12020PHP/week6/class/User.php
@@ -27,5 +27,38 @@ class User
         $this->name = $name;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username; //this mean his
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+//they are my methods
+
 
 }
